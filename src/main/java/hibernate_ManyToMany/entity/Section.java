8e648475a37a65366c6b5b318dd548cfbaf3e103
Liudmila-Topal <prepelitsa.liudmila@gmail.com>
2024-01-10 19:27:19 +0200

    Many to many

---
+++ src/main/java/hibernate_ManyToMany/entity/Section.java
@@ -0,0 +1,53 @@
+package hibernate_ManyToMany.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "section")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.REFRESH,
+            CascadeType.DETACH})
+    @JoinTable(name = "child_section",
+            joinColumns = @JoinColumn(name = "section_id"),
+            inverseJoinColumns = @JoinColumn(name = "child_id"))
+    private List<Child> children;
+
+    public Section(String name) {
+        this.name = name;
+    }
+
+    public void addChildToSection(Child child) {
+        if (children == null) {
+            children = new ArrayList<>();
+        }
+        children.add(child);
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
